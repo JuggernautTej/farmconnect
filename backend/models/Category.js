1d87d25e3d9eb7179d5f846394b0ea33c5c24808
JuggernautTej <tiwatej@gmail.com>
2025-01-14 15:45:52 +0100

    modified the Category js file

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-    name: { type: Strinf, required : true, unique: true },
+    name: { type: String, required : true, unique: true },
     description: { type: String, required: true },
 }, { timestamps: true });
 
